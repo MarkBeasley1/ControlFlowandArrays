01a6cc887338fc799ef01be75eb907b612afe5bd
MarkBeasley1 <marquettebeasley@yahoo.com>
2022-11-03 14:13:24 -0600

    created express server

--- CSS-Practice/quotes.js
+++ CSS-Practice/quotes.js
@@ -1,10 +1,30 @@
 let quotes = [
-  `The hallmark of a real man is controlling himself, controlling his emotions, and acting appropriately regardless of how he feels.`,
-    `There is an elite club that youre not in and none of them people are arguing about the dumb sh*t youre arguing about.`,
+    `The hallmark of a real man is controlling himself, controlling his emotions, and acting appropriately regardless of how he feels.`,
+    `There is an elite club that youre not in and none of those people are arguing about the dumb sh*t youre arguing about.`,
     `I will never apologise because I dont make mistakes.`,
     `Its just about if youre high enough you can throw it all away… If corruption exists, which it does, let us all play.`,
     `What color is your Bugatti?`,
-    `How to be successful as a man… First thing, you need freedom cus you aint a man if youre not free.`
+    `How to be successful as a man… First thing, you need is freedom cus you aint a man if youre not free.`,
+    `I am the only complete man. I have it all. A knight means nothing without the bishops and rooks. You must have everything. You must be a professional.`,
+    `Get important people to recognize your face. That's your way out.`,
+    `Stop aiming for the giddish childish joy of a Christmas morning. We're adults. Just be satisfied nobody you love has died today.`,
+    `Have you taken advantage of the few things you actually control? Or still hoping to just get lucky?`,
+    `How many actually rich people are you speaking to daily? None? One? That is what will actually save you from a slave's existence. Rewire your brain.`,
+    `I find that very few men really want freedom. They'd rather play it safe. And take the highest hourly wage they can get their hands on. Hoping that will make them rich when the math clearly doesn't add up.`,
+    `Youre hoping and waiting. Thats a dream. I had a plan. Not dreams.`,
+    `If the punishment for a 'crime' is a fine. Then it's an activity which is legal for a price.`,
+    `If you're known as crazy. Your life is going to go one of two ways. Absolute success, or the absolute bottom. But you can rest assured you most likely won't end up average like the rest`,
+    `Listen to rich people. It's that simple.`,
+    `Most of you have been down so long that getting up hasn't crossed your mind.`,
+    `No exceptional person ever lived like an average person.`,
+    `My dad always used to say 'outstanding in the rain' as opposed to simply 'outstanding' when he was impressed.
+
+    I asked him: Dad, why the rain?
+    
+    And he replied: Son, any jackass can perform when the sun is shining.`,
+    `It's who you know that makes what you know important.`,
+    `Lacking motivation is not a real problem. Only absolute losers 'suffer' with the inability to strive to become something`,
+
   ];
   
   document.addEventListener("DOMContentLoaded", function(event) {
@@ -13,10 +33,7 @@ let quotes = [
       document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
     };
     randomQuote();
-  
-    
-  
-    
+      
   document.querySelector('#main-title').textContent = 'Top G Speaks'
     
   document.querySelector('body').style.backgroundColor = 'blue'
