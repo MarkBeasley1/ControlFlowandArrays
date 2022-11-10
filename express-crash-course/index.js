dc41958fc3431be751e625961f4e83453780f32b
MarkBeasley1 <marquettebeasley@yahoo.com>
2022-11-10 10:48:30 -0700

    my changes

--- express-crash-course/index.js
+++ express-crash-course/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-const app = require('process');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+
+//init middleware
+// app.use(logger);
+
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+appset('view engine', 'handlebars');
+
+
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+//hompage route
+app.get('/', (req, res) => res.render('index',{
+    title: 'Member App',
+    members
+}));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+//members api route
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.port || 5000;
 
